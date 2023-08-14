fdbff7cd16e9b9e012f4aefb44e583d572beeff0
Gregorio Toscano <gtoscano@gmail.com>
2023-08-14 15:46:56 -0400

    Update in url

--- factorial_project/factorial_app/urls.py
+++ factorial_project/factorial_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FactorialView
 
 urlpatterns = [
-    path('factorial/<int:number>/', FactorialView.as_view(), name='factorial'),
+    path('factorial/<int:number>', FactorialView.as_view(), name='factorial'),
 ]
